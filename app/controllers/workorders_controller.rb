288667861ef7503b8472ee71b30f5b55f770afb1
AndreaJasper <andrea.nicole.jasper@gmail.com>
2020-08-20 18:47:10 -0600

    adding new features to app and debugging

--- app/controllers/workorders_controller.rb
+++ app/controllers/workorders_controller.rb
@@ -18,10 +18,10 @@ class WorkordersController < ApplicationController
     
     post '/workorders' do
       if logged_in?
-        if params[:description] == ""
+        if params[:content] == ""
           redirect to "/workorders/new"
         else
-          @workorder = current_user.workorders.build(description: params[:description])
+          @workorder = current_user.workorders.build(content: params[:content])
           if @workorder.save
             redirect to "/workorders/#{@workorder.id}"
           else
@@ -57,7 +57,7 @@ class WorkordersController < ApplicationController
     
     patch '/workorders/:id' do
       if logged_in?
-        if params[:description] == ""
+        if params[:content] == ""
           redirect to "/workorders/#{params[:id]}/edit"
         else
           @workorder = Workorder.find_by_id(params[:id])
