10ce5451173947fa29fb0e4e2ddb5c3daf4f7183
Andrea <andrea.nicole.jasper@gmail.com>
2021-06-04 13:28:47 -0600

    updated readme file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,18 @@ class UsersController < ApplicationController
       @user = User.find_by_slug(params[:slug])
       erb :'users/show'
     end
-    
+
     get '/signup' do
       if !logged_in?
         erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
       else
         redirect to '/workorders'
       end
-    end  
-    
+    end
+
     post '/signup' do
       if params[:username] == "" || params[:email] == "" || params[:password] == ""
-        redirect to '/signup'      
+        redirect to '/signup'
       else
         @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
         @user.save
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
         redirect to '/workorders'
       end
     end
-    
+
     get '/login' do
       if !logged_in?
         erb :'users/login'
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
         redirect to '/workorders'
       end
     end
-    
+
     post '/login' do
       user = User.find_by(:username => params[:username])
       if user && user.authenticate(params[:password])
@@ -40,8 +40,8 @@ class UsersController < ApplicationController
         redirect to '/signup'
       end
     end
-    
-      get '/logout' do
+
+    get '/logout' do
       if logged_in?
         session.destroy
         redirect to '/login'
